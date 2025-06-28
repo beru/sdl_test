a4c6077d3ef0fa0436d729b542d4125186f19700
katsuhisa yuasa <berupon@gmail.com>
2025-06-28 19:38:14 +0900

    use spng and miniz

--- main.cpp
+++ main.cpp
@@ -13,18 +13,56 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-static SDL_Window* window = NULL;
-static SDL_Renderer* renderer = NULL;
+#include <filesystem>
+namespace fs = std::filesystem;
+
+#include "spng/spng.h"
+#include <io.h>
+
+namespace {
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+SDL_Texture* texture = NULL;
+std::vector<char> image_data;
+
+}
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
     /* Create the window */
-    if (!SDL_CreateWindowAndRenderer("Hello World", 800, 600, SDL_WINDOW_FULLSCREEN, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("Hello World", 800, 600, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
         SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
     return SDL_APP_CONTINUE;
+}
+
+void loadPNG(fs::path& path)
+{
+    FILE* f = _wfopen(path.c_str(), L"rb");
+    if (!f) return;
+    const size_t len = _filelength(_fileno(f));
+    std::vector<char> buff(len);
+    fread(buff.data(), 1, len, f);
+    fclose(f);
+
+    spng_ctx* ctx = spng_ctx_new(0);
+    spng_set_png_buffer(ctx, buff.data(), len);
+    spng_ihdr ihdr;
+    spng_get_ihdr(ctx, &ihdr);
+    size_t out_size;
+    const int fmt = SPNG_FMT_RGBA8;
+    spng_decoded_image_size(ctx, fmt, &out_size);
+    image_data.resize(out_size);
+    spng_decode_image(ctx, image_data.data(), out_size, fmt, 0);
+    spng_ctx_free(ctx);
+
+    SDL_Surface* surface = SDL_CreateSurfaceFrom(ihdr.width, ihdr.height, SDL_PIXELFORMAT_ABGR8888, image_data.data(), ihdr.width * 4);
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_DestroySurface(surface);
+
 }
 
 /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
@@ -33,6 +71,13 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
     if (event->type == SDL_EVENT_KEY_DOWN ||
         event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+    }
+    else if (event->type == SDL_EVENT_DROP_FILE) {
+        SDL_DropEvent* dropEvent = (SDL_DropEvent*)event;
+        fs::path data{ dropEvent->data };
+        if (data.extension() == ".png") {
+            loadPNG(data);
+        }
     }
     return SDL_APP_CONTINUE;
 }
@@ -54,6 +99,9 @@ SDL_AppResult SDL_AppIterate(void* appstate)
     /* Draw the message */
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
+    if (texture != nullptr) {
+        SDL_RenderTexture(renderer, texture, NULL, NULL);
+    }
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderDebugText(renderer, x, y, message);
     SDL_RenderPresent(renderer);
@@ -64,5 +112,9 @@ SDL_AppResult SDL_AppIterate(void* appstate)
 /* This function runs once at shutdown. */
 void SDL_AppQuit(void* appstate, SDL_AppResult result)
 {
+    if (texture != nullptr) {
+        SDL_DestroyTexture(texture);
+        texture = nullptr;
+    }
 }
 
